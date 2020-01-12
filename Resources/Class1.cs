54332c690d5d2558fa8a2710cbe566f60058edff
Anastasiia <a.khtystova@gmail.com>
2020-01-12 11:21:47 +0200

    Finish

---
+++ Resources/Class1.cs
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+﻿using System;
+
+ namespace Resources
+{
+    public class Class1
+    {
+    }
+}
+=======
+﻿using System;
+
+ namespace Resources
+{
+    public class Class1
+    {
+    }
+}
+>>>>>>> 286ad2923df5308a4216bd0d9c75d39f78976ade
